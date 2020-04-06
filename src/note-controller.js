671fcc4ef991ad8cb1236e5a629c075e23d63bca
CodeKrakken <newholm1@hotmail.com>
2020-04-06 21:58:45 +0100

    Add display notes function

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,12 +1,18 @@
 (function(exports) {
-  function NoteController() {
-    this.notelist = new NoteList;
-    this.notelistview = new NoteListView(this.notelist)
+  function NoteController(notelist) {
+    this.notelist = notelist;
+    this.notelist.createNote("Favourite drink: seltzer, after the salsa");
+    this.notelistview = new NoteListView(this.notelist);
   }
 
   NoteController.prototype.howdify = function() {
     var element = document.getElementById("app");
     element.innerHTML = "Howdy";
+  }
+
+  NoteController.prototype.displayNotes = function() {
+    var element = document.getElementById("app");
+    element.innerHTML = this.notelistview.returnHTML();
   }
 
   exports.NoteController = NoteController;
