00e2ae161afde5bf841457757235b157fdced32d
CodeKrakken <newholm1@hotmail.com>
2020-04-06 17:46:02 +0100

    Returns a two item list with HTML

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -1,8 +1,8 @@
 (function() {
   function testStoresANoteListModel() {
-  var notelist = new NoteList;
-  notelist.createNote("I LIVE");
-  var notelistview = new NoteListView(notelist);
+    var notelist = new NoteList;
+    notelist.createNote("I LIVE");
+    var notelistview = new NoteListView(notelist);
     assert.isTrue(notelistview.notelist.notes[0].text === "I LIVE");
   }
 
@@ -11,11 +11,23 @@
 
 (function() {
   function testReturnsSingleItemNoteListWithHTML() {
-  var notelist = new NoteList();
-  notelist.createNote("I LIVE");
-  var notelistview = new NoteListView(notelist);
-  assert.isTrue(notelistview.returnHTML() === '<div align="center"><ul><li>I LIVE</li></ul></div>');
+    var notelist = new NoteList;
+    notelist.createNote("I LIVE");
+    var notelistview = new NoteListView(notelist);
+    assert.isTrue(notelistview.returnHTML() === '<div align="center"><ul><li>I LIVE</li></ul></div>');
   }
 
   testReturnsSingleItemNoteListWithHTML();
+})(this);
+
+(function() {
+  function testReturnsTwoItemNoteListWithHTML() {
+    var notelist = new NoteList;
+    notelist.createNote("One");
+    notelist.createNote("Two");
+    var notelistview = new NoteListView(notelist);
+    assert.isTrue(notelistview.returnHTML() === '<div align="center"><ul><li>One</li><li>Two</li></ul></div>')
+  }
+
+  testReturnsTwoItemNoteListWithHTML();
 })(this);
