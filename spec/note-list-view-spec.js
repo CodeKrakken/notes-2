b48038536fea753bcf30d6d6ca441371d2500770
CodeKrakken <newholm1@hotmail.com>
2020-04-06 14:06:05 +0100

    Return a single item notelist with HTML

--- spec/note-list-view-spec.js
+++ spec/note-list-view-spec.js
@@ -1,10 +1,21 @@
 (function() {
   function testStoresANoteListModel() {
-    var notelist = new NoteList;
-    notelist.createNote("I LIVE");
-    var notelistview = new NoteListView(notelist);
+  var notelist = new NoteList;
+  notelist.createNote("I LIVE");
+  var notelistview = new NoteListView(notelist);
     assert.isTrue(notelistview.notelist.notes[0].text === "I LIVE");
   }
 
   testStoresANoteListModel();
+})(this);
+
+(function() {
+  function testReturnsSingleItemNoteListWithHTML() {
+  var notelist = new NoteList();
+  notelist.createNote("I LIVE");
+  var notelistview = new NoteListView(notelist);
+  assert.isTrue(notelistview.returnHTML() === '<div align="center"><ul><li>I LIVE</li></ul></div>');
+  }
+
+  testReturnsSingleItemNoteListWithHTML();
 })(this);
--- src/note-list-view.js
+++ src/note-list-view.js
@@ -3,5 +3,10 @@
     this.notelist = notelist
   }
 
+  NoteListView.prototype.returnHTML = function() {
+    this.notelist.notes.forEach(note => this.notelist.texts.push(note.text));
+    return '<div align="center"><ul><li>' + this.notelist.texts.join('</li><li>') + '</li></ul></div>';
+  }
+
   exports.NoteListView = NoteListView
 })(this);
